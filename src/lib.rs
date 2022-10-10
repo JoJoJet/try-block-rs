c4c83af22f8e66a599c46efe81d9b9a85e8b9699
JoJoJet <21144246+JoJoJet@users.noreply.github.com>
2022-10-10 19:23:04 -0400

    Mark the crate as deprecated

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 #![no_std]
+//! # Notice
+//!
+//! This crate has been moved/renamed to [tryvial](https://github.com/JoJoJet/tryvial).
+//! You should use that instead.
+//!
+//! ---
+//!
 //! Macro for stable try blocks that performs Ok-wrapping, and otherwise tries to
 //! achieve feature parity with RFC 1859. The macro is compatible with any type
-//! that implements the unstable `Any` trait through the use of type magic.
-//!
-//! This crate is a fork of `try-block`, which has not been updated in four years at
-//! the time of writing this. This fork adds Ok-wrapping and the promise of future
-//! updates.
+//! that implements the unstable `Try` trait through the use of type magic.
 //!
 //! This crate is `no_std` compatible.
 
